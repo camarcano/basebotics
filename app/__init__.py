5a74d7ffe6c35bba96b7c7b9abfc368a4d6e9ea7
camarcano <c.marcano@gmail.com>
2024-12-24 01:39:26 -0600

    .

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,7 @@ from .routes.dash import dashboard_bp, init_dash_app
 
 def create_app():
     app = Flask(__name__)
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://carlos:yourpassword@localhost/balldatalab'
     app.config['SECRET_KEY'] = 'rickeyhenderson'
-
-    # Initialize extensions
-    # from .extensions import db  # Make sure db is defined in extensions
-    # db.init_app(app)
 
     # Register blueprints
     from .routes.base import base_bp
@@ -28,7 +23,7 @@ def create_app():
     handler.setLevel(logging.INFO)
     app.logger.addHandler(handler)
 
-    @app.before_first_request
+    @app.before_request
     def initialize_logging():
         app.logger.info('Flask app started')
 
@@ -43,4 +38,3 @@ def create_app():
         return render_template('500.html'), 500
 
     return app
-
