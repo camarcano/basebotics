861c7ddad467b61f8b6a2d8cb2d666d1452e4cd3
camarcano <c.marcano@gmail.com>
2024-12-24 01:23:55 -0600

    .

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
+import logging
+from logging.handlers import RotatingFileHandler
 from .routes.dash import dashboard_bp, init_dash_app
 
 def create_app():
@@ -22,3 +24,22 @@ def create_app():
     init_dash_app(app)
 
     return app
+
+# Set up logging
+handler = RotatingFileHandler('flask_app.log', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.INFO)
+app.logger.addHandler(handler)
+
+@app.before_first_request
+def initialize_logging():
+    app.logger.info('Flask app started')
+
+@app.errorhandler(500)
+def internal_error(error):
+    app.logger.error('Server Error: %s', (error))
+    return render_template('500.html'), 500
+
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+    app.logger.error('Unhandled Exception: %s', (e))
+    return render_template('500.html'), 500
--- run.py
+++ run.py
@@ -1,27 +1,7 @@
 from .app import create_app
-import logging
-from logging.handlers import RotatingFileHandler
 
 app = create_app()
 
-# Set up logging
-handler = RotatingFileHandler('flask_app.log', maxBytes=10000, backupCount=1)
-handler.setLevel(logging.INFO)
-app.logger.addHandler(handler)
-
-@app.before_first_request
-def initialize_logging():
-    app.logger.info('Flask app started')
-
-@app.errorhandler(500)
-def internal_error(error):
-    app.logger.error('Server Error: %s', (error))
-    return render_template('500.html'), 500
-
-@app.errorhandler(Exception)
-def unhandled_exception(e):
-    app.logger.error('Unhandled Exception: %s', (e))
-    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run()
